859d469b0a8c13b4ab201a519ae83a49dabf597d
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-12-16 14:16:14 +0300

    Add project files.

---
+++ CSharpEgitimKampi501/Dtos/CreateProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.Dtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductStock { get; set; }
+        public string ProductPrice { get; set; }
+    }
+}
